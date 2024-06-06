ff43221c0457994957009e9c685f2604301e1171
Gloria Wekura <wekuraglorian07@gmail.com>
2024-06-06 17:24:31 +0300

    submit lab

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,21 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__(self, name="", breed=""):
+        if name:
+            self.set_name(name)
+        if breed:
+            self.set_breed(breed)
+
+    def set_name(self, name):
+        if not isinstance(name, str) or not (1 <= len(name) <= 25):
+            print("Name must be string between 1 and 25 characters.")
+        else:
+            self.name = name
+
+    def set_breed(self, breed):
+        approved_breeds = ["Pug", "Beagle", "Bulldog", "Labrador", "Golden Retriever"]
+        if breed not in approved_breeds:
+            print("Breed must be in list of approved breeds.")
+        else:
+            self.breed = breed
--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,21 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    def __init__(self, name="", job=""):
+        if name:
+            self.set_name(name)
+        if job:
+            self.set_job(job)
+
+    def set_name(self, name):
+        if not isinstance(name, str) or not (1 <= len(name) <= 25):
+            print("Name must be string between 1 and 25 characters.")
+        else:
+            self.name = name.title()  # Convert to title case
+
+    def set_job(self, job):
+        approved_jobs = ["Sales", "Engineer", "Doctor", "ITC"]
+        if job not in approved_jobs:
+            print("Job must be in list of approved jobs.")
+        else:
+            self.job = job
